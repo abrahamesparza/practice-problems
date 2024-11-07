ee46ad7464562a8b086afd3fce0f46cb17f30743
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-11-06 16:58:22 -0800

    Clean notes

--- javascript/majorityElement.js
+++ javascript/majorityElement.js
@@ -1,4 +1,5 @@
-/* LEET CODE CHALLENGE (easy)
+/*--------------
+LEET CODE CHALLENGE (easy)
 Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times.
@@ -12,24 +13,30 @@ Example 2:
 
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
-*/
+--------------*/
+
+/*------------*/
+//Dissecting the problem:
+/*
+I: an array of numbers
+O: the element in the array that appears
+    more than (nums.length / 2)
+C: if length of array is 1 return the number in the array
+
+
+
+//Approach:
+//i want to solve this by storing each number in the array
+//as the key in a dictionary, and the count of appearances as the value
+//then i want to iterate through that dictionary, and use the value to 
+//check which key appears more times than the threshold
+//and return that key
+/*------------*/
 
 var majorityElement = function (nums) {
-    /*
-    I: an array of numbers
-    O: the number in the array that appears
-       more than the length of the array / 2
-    C: if length of array is 1 return the number in the array
-    */
     let n = nums.length;
-    let threshold = n / 2;
-  
-    //i want to solve this by storing each number in the array
-    //as the key in a dictionary, and the count of appearances as the value
-    //then i want to iterate through that dictionary, and use the value to 
-    //check which key appears more times than the threshold
-    //and return that key
-  
+    if (n === 1) return nums[0];
+
     let numsMap = new Map();
     for (let num of nums) {
       if (!numsMap.has(num)) {
@@ -37,7 +44,8 @@ var majorityElement = function (nums) {
       }
       numsMap.set(num, (numsMap.get(num)) + 1)
     }
-  
+    
+    let threshold = n / 2;
     for (let [key, value] of numsMap.entries()) {
       if (value < threshold) {
         continue;
